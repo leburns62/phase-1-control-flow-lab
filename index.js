c0e0eae06c50474ca9d16fba15f9d53a17b70f3c
Levi Hutchinson Burns <leburns62@gmail.com>
2022-04-26 21:43:56 -0400

    commit tree a07adccfc9d71846b01f68dc7edeb587fe08f874

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){;
+  if(feet <= 400){
+    return "This one is on me!";
+  }
+  else if( (400 < feet) && (feet < 2000) ) {
+    return ("That will be twenty bucks.");
+  }
+  else if( (feet >= 2000) && (feet < 2500) ){
+    return ("I will gladly take your thirty bucks.");
+  }
+  else if(feet >= 2500) {
+    return("No can do.");
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return(city === "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generous){
   // Write your code here!
+  switch(generous) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.'
+      break;
+  }
 }
